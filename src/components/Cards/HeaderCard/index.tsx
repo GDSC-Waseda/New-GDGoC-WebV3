71dd390073f293ca6f915e35d694fe35b5235e3a
Ayana326 <ayana_u@akane.waseda.jp>
2025-06-17 23:57:53 +0900

    run prettire

--- src/components/Cards/HeaderCard/index.tsx
+++ src/components/Cards/HeaderCard/index.tsx
@@ -1,7 +1,6 @@
 import { HeaderCardProps } from "~/types/index";
 
 export const HeaderCard: React.FC<{ props: HeaderCardProps }> = ({ props }) => {
-
   return (
     <div className="headerCard">
       <h1 className="headerCard__title">{props.title}</h1>
