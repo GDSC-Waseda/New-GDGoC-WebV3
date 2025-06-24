10191d72b728ec2efc342168426aa3f9b5d6e4eb
Ayana326 <ayana_u@akane.waseda.jp>
2025-06-24 19:54:30 +0900

    fix layout

--- src/components/Cards/HeaderCard/index.tsx
+++ src/components/Cards/HeaderCard/index.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 
 export const HeaderCard: React.FC<{ props: HeaderCardProps }> = ({ props }) => {
   return (
-      <Card className="headerCard">
-        {props.headTitle && (
-          <Card.Title className="headerCard__headerTitle">
-            {props.headTitle}
-          </Card.Title>
-        )}
-        <div className="headerCard__container">
-          <Card.Text className="headerCard__title">{props.title}</Card.Text>
-        </div>
-        {props.content &&
-          props.content.split("\n").map((text, key) => (
-            <p key={key} className="headerCard__contents">
-              {text}
-            </p>
-          ))}
-      </Card>
+    <Card className="headerCard">
+      {props.headTitle && (
+        <Card.Title className="headerCard__headerTitle">
+          {props.headTitle}
+        </Card.Title>
+      )}
+      <div className="headerCard__container">
+        <Card.Text className="headerCard__title">{props.title}</Card.Text>
+      </div>
+      {props.content &&
+        props.content.split("\n").map((text, key) => (
+          <p key={key} className="headerCard__contents">
+            {text}
+          </p>
+        ))}
+    </Card>
   );
 };
 
