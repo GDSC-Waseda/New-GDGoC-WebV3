8c53ed46101fe2038adc7a1030710f77bc83aad3
Ayana326 <ayana_u@akane.waseda.jp>
2025-06-24 15:00:03 +0900

    change logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,48 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import X from "assets/svg/X.svg";
 import Instagram from "assets/svg/instagram.svg";
 import LinkedIn from "assets/svg/linked-in.svg";
 import Mail from "assets/svg/mail.svg";
 import logo from "assets/svg/logo.svg";
+import { FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export const Footer = (): JSX.Element => {
   return (
     <footer className="footer">
       <div className="footer__reserved">Contact</div>
       <div className="footer__logoContainer">
-        <a
-          href="https://x.com/gdsc_waseda"
-          target="_blank"
-          className="footer__pathItem"
-          rel="noreferrer"
-        >
-          <Image src={X} layout="intrinsic" alt="X" />
+        <a href="https://x.com/gdsc_waseda" target="_blank" rel="noreferrer">
+          <FaXTwitter size={48} />
         </a>
         <a
           href="https://www.instagram.com/gdgcampus_waseda/"
-          className="footer__pathItem"
           target="_blank"
           rel="noreferrer"
         >
-          <Image src={Instagram} layout="intrinsic" alt="instagram" />
+          <FaInstagram size={48} />
         </a>
         <a
           href="https://www.linkedin.com/company/google-dsc-waseda-university"
-          className="footer__pathItem"
           target="_blank"
           rel="noreferrer"
         >
-          <Image src={LinkedIn} layout="intrinsic" alt="linkdin" />
+          <FaLinkedin size={48} />
         </a>
         <a
           href="https://www.linkedin.com/company/google-dsc-waseda-university"
-          className="footer__pathItem"
           target="_blank"
           rel="noreferrer"
         >
-          <Image src={Mail} layout="intrinsic" alt="mail" />
+          <FaEnvelope size={48} />
         </a>
       </div>
       <div className="footer__reserved">@ GDGoC Waseda</div>
