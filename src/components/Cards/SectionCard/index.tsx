813b9e8782cb885b32d5e7dfcf6765ce70060644
Ayana326 <ayana_u@akane.waseda.jp>
2025-07-01 00:01:51 +0900

    add team sub-text, change css to use tailwind

--- src/components/Cards/SectionCard/index.tsx
+++ src/components/Cards/SectionCard/index.tsx
@@ -4,9 +4,13 @@ export const SectionCard: React.FC<{ props: SectionCardProps }> = ({
   props,
 }) => {
   return (
-    <div className="sectionCard">
-      <h1 className="sectionCard__title">{props.title}</h1>
-      <p className="sectionCard__contents">{props.content}</p>
+    <div className="my-5 mx-16 flex justify-center">
+      <div className="w-full">
+        <h1 className="text-center text-[max(2vw,33px)]">{props.title}</h1>
+        {props.content && (
+          <p className="pt-2 text-center text-[#404040]">{props.content}</p>
+        )}
+      </div>
     </div>
   );
 };
