864a32b6e7f4ae29b798ec8048382c999cb8ac91
nicknr100 <nicknr100@gmail.com>
2025-07-04 17:53:11 +0900

    Home page initial version

---
+++ src/components/Cards/HomeProjectCard/index.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export interface ProjectCardProps {
+  title: string;
+  description: string;
+  imageUrl?: string;
+  repoUrl: string;
+  team: string;
+  projectId: string;
+  members?: string[];
+}
+
+const HomeProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  imageUrl,
+  repoUrl,
+  members,
+}) => {
+  return (
+    <div className="homeProjectCard">
+      {imageUrl && (
+        <div className="imageContainer">
+          <Image
+            src={imageUrl}
+            alt={`${title} image`}
+            layout="fill"
+            objectFit="cover"
+            className="image"
+          />
+        </div>
+      )}
+      <div className="cardBody">
+        <h5 className="title">{title}</h5>
+        <p className="description">{description}</p>
+        {members && (
+          <div className="contributors">
+            <strong>Contributors:</strong>
+            <ul>
+              {members.map((member, index) => (
+                <li key={index}>
+                  {member}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <Link href={repoUrl} className="githubLink">
+          View on GitHub
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomeProjectCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,107 @@ import type { GetStaticProps, GetStaticPropsContext } from "next";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { HomeMediaCard } from "components/Cards/HomeMediaCard";
+import HomeProjectCard from "components/Cards/HomeProjectCard";
+import Link from "next/link";
+import { client } from "../sanity";
+import { MediaCardProps, ProjectCardProps } from "~/types";
 
 export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext
 ) => {
   const { locale } = context;
+  const query = `*[_type == "blogPost"]{
+    title,
+    "imageUrl": mainImage.asset->url,
+    tags,
+    publishedAt,
+    shortDesc,
+    slug
+  }`;
+  const blogPostsResponse = await client.fetch(query);
+  const blogPosts = blogPostsResponse.map(
+    (post: {
+      title: string;
+      imageUrl: any;
+      tags: string[];
+      publishedAt: string;
+      shortDesc: string;
+      slug: {
+        current: string;
+      };
+    }) => ({
+      size: "m",
+      title: post.title,
+      image: post.imageUrl,
+      tags: post.tags,
+      date: new Date(post.publishedAt).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+      description: post.shortDesc,
+      // to do fix link
+      link: "/events/details/mini-solution-challenge-2023/",
+      open: true,
+      canOpen: false,
+    })
+  );
 
   return {
     props: {
-      ...(await serverSideTranslations(locale as string, ["home", "common"])),
+      ...(await serverSideTranslations(locale as string, ["home", "common", "events"])),
+      blogPosts
     },
   };
 };
 
-const Home: NextPage = () => {
+const Home: NextPage<{ blogPosts: MediaCardProps[] }> = ({
+  blogPosts,
+}) => {
   const { t } = useTranslation();
   const [loading, setLoading] = useState(true);
+  const [blogPost, setBlogPosts] = useState<MediaCardProps[]>(blogPosts);
+
+  const filterPastEvents = (input: string) => {
+    return blogPost.filter(
+      (event) =>
+        event.title.toLowerCase().includes(input.toLowerCase()) ||
+        event.description.toLowerCase().includes(input.toLowerCase())
+    );
+  };
+
+  const sampleProjects: ProjectCardProps[] = [
+    {
+      title: "WasedaLine1",
+      description: "A queue tracking project for university",
+      imageUrl: "/wasedaLine.jpg",
+      repoUrl: "https://github.com/s3nmith/WasedaLineWeb",
+      team: "Team 1",
+      projectId: "wasedaline01",
+      members: ["Lahiru Udawatta"],
+    },
+    {
+      title: "WasedaLine2",
+      description: "A queue tracking project for university",
+      imageUrl: "/wasedaLine.jpg",
+      repoUrl: "https://github.com/s3nmith/WasedaLineWeb",
+      team: "Team 3",
+      projectId: "wasedaline03",
+      members: ["Lahiru Udawatta"],
+    },
+    {
+      title: "WasedaLine3",
+      description: "A queue tracking project for university",
+      imageUrl: "/wasedaLine.jpg",
+      repoUrl: "https://github.com/s3nmith/WasedaLineWeb",
+      team: "Team 3",
+      projectId: "wasedaline03",
+      members: ["Lahiru Udawatta"],
+    },
+  ];
+
+  const [projects, setProjects] = useState<ProjectCardProps[]>(sampleProjects);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -36,68 +121,98 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="container">
+    <>
       <CommonMeta pageTitle="Home" />
-      <div className="home-page">
-        <div className="home-page__top">
-          <div className="home-page__top__container">
-            <div className="content-with-image">
-              <div className="text-section">
-                <div className="home-page__top__title">
-                  {t("home:welcome_mes")}
-                </div>
-              </div>
-              <div className="image-section">
-                <Image
-                  src="/gdsc-top.jpg"
-                  alt="women-tech"
-                  width={650}
-                  height={400}
-                  layout="intrinsic"
-                  className="animated-image"
-                />
-              </div>
-            </div>
+      <div className="home-section section-1">
+        <div className="title-container">
+          <div className="page-title">
+            Google Developer <br /> Groups <br /> on Campus
           </div>
-          <p className="trusted-message">{t("home:trusted_by")}</p>
-          <div className="trusted-companies">
-            <Image
-              src="/women_tech.jpg"
-              alt="women-tech"
-              width={100}
-              height={100}
-              className="animated-image"
-              layout="intrinsic"
-            />
-            <Image
-              src="/google.jpg"
-              alt="google"
-              width={100}
-              height={100}
-              className="animated-image"
-              layout="intrinsic"
-            />
-            <Image
-              src="/fingate.jpg"
-              alt="fingate"
-              width={100}
-              height={100}
-              className="animated-image"
-              layout="intrinsic"
-            />
-            <Image
-              src="/fincatch.jpg"
-              alt="fincatch"
-              width={100}
-              height={100}
-              className="animated-image"
-              layout="intrinsic"
-            />
+        </div>
+        <div className="subtitle-container">
+          <div className="subtitle">
+            Waseda University
           </div>
         </div>
       </div>
-    </div>
-  );
+      <div className="home-section section-2">
+        <div className="description-container">
+          <div className="header">
+            {t("home:about_us")}
+          </div>
+          <div className="separator" />
+          <div className="description-text">
+            {t("home:about_us_text")}
+          </div>
+          <Link href="/about" className="learn-more-button">
+            {t("home:learn_more")}
+          </Link>
+        </div>
+      </div>
+      <div className="home-section section-3">
+        <div className="description-container">
+          <div className="header">
+            {t("home:teams")}
+          </div>
+          <div className="separator" />
+          <div className="description-text">
+            {t("home:teams_text")}
+          </div>
+          <Link href="/teams" className="learn-more-button">
+            {t("home:learn_more")}
+          </Link>
+        </div>
+      </div>
+      <div className="home-section section-4">
+        <div className="page-container">
+          <div className="top-container">
+            <div className="header">
+              {t("home:projects")}
+            </div>
+            <div className="separator" />
+            <div className="brief-text">
+              {t("home:projects_text")}
+            </div>
+          </div>
+
+          <div className="project-cards-container">
+            {projects.map((project, index) => (
+              <HomeProjectCard key={index} {...project} />
+            ))}
+          </div>
+
+          <Link href="/project" className="learn-more-button">
+            {t("home:learn_more")}
+          </Link>
+        </div>
+      </div>
+      <div className="home-section section-5">
+        <div className="page-container">
+          <div className="top-container">
+            <div className="header">
+              {t("home:events")}
+            </div>
+            <div className="separator" />
+            <div className="brief-text">
+              {t("home:events_text")}
+            </div>
+          </div>
+
+          <div className="cards-container">
+            {blogPost.map((post, index) => (
+              <a href={post.link} key={index}>
+                <HomeMediaCard props={post} />
+              </a>
+            ))}
+          </div>
+
+          <Link href="/events" className="learn-more-button">
+            {t("home:learn_more")}
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Home;
